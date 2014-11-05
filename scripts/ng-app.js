83f5a4a70a69ab84148c4c77a34bfe4d7253bfbc
Garry West <GarryWest@msn.com>
2014-11-05 17:41:14 -0600

    Added multiple views

--- scripts/ng-app.js
+++ scripts/ng-app.js
@@ -1,5 +1,12 @@
 angular.module('ng-app', [])
     .controller('ng-controller', ['$scope', function($scope) {
     	$scope.genderMale = "true";
+    	$scope.needWords = "true";
+    	$scope.submit = function(){
+    		$scope.needWords = "false";
+    	};
+    	$scope.startOver = function(){
+    		$scope.needWords = "true";
+    	};
     }]);
 
